d18a838e4338a535fa2313db1000af5d9060b4d2
marchik88 <marchik88@rambler.ru>
2018-06-04 14:18:50 +0300

    added google tag manager

---
+++ build/contact/contact.php
@@ -0,0 +1,25 @@
+<?php
+
+$text = "<span style='color:red; font-size: 35px; line-height: 40px; magin: 10px;'>Error! Please try again.</span>";
+
+if(isset($_POST['submitcontact']))
+{
+	$name=$_POST['name'];
+	$email=$_POST['email'];
+	$message=$_POST['message'];
+	$subject=$_POST['subject'];
+
+	$to = "m.stebko@gmail.com";
+	$subject = "Zerotheme - Testing Contact Form";
+	$message = " Name: " . $name ."\r\n Email: " . $email . "\r\n Message:\r\n" . $message;
+	 
+	$from = "Zerotheme dot com";
+	$headers = "From:" . $from . "\r\n";
+	$headers .= "Content-type: text/plain; charset=UTF-8" . "\r\n"; 
+	 
+	if(@mail($to,$subject,$message,$headers))
+	{
+	  header("Location: ../success/index.html");
+	}
+}
+?>
